4ed77a75d850f779420fb527b464516b465ae4e9
ExtremePro11299 <134209046+ExtremePro11299@users.noreply.github.com>
2023-05-27 18:21:15 +0200

    Add files via upload

---
+++ Resources/Traceroute.ps1
@@ -0,0 +1,36 @@
+function startP()
+{
+"Would you like to save the results to a file?"
+"1) No"
+"2) Yes"
+$global:savetype = Read-Host
+testprep
+}
+
+function testprep()
+{
+"Please enter the IP address you would like to trace the route to."
+$global:ip = Read-Host
+test
+}
+
+function test()
+{
+cd..
+if ( 1 -eq $savetype ){tracert $ip}
+elseif ( 2 -eq $savetype ) 
+{
+$fileid = Get-Random -Minimum 1111 -Maximum 9999
+tracert $ip >> SavedResults\TraceResult-$fileid.txt
+}
+"What do you want to do? type the number"
+"1) Trace again"
+"2) Change settings"
+"3) Exit"
+$einput = Read-Host
+if ( 1 -eq $einput ){testprep}
+elseif ( 2 -eq $einput ){startP}
+elseif ( 3 -eq $einput ){exit}
+}
+$host.UI.RawUI.WindowTitle = "Route tracer"
+startP
