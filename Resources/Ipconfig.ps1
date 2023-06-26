1bcfd7aee6c1e98fb76ccfe35272d0b78609cf28
ExtremePro11299 <134209046+ExtremePro11299@users.noreply.github.com>
2023-06-26 09:11:45 +0200

    Add ipconfig /flushdns

--- Resources/Ipconfig.ps1
+++ Resources/Ipconfig.ps1
@@ -3,6 +3,7 @@ function startP()
 "How would you like to Ipconfig?"
 "1) Show info"
 "2) Show detailed info"
+"3) Flush DNS resolver cache"
 $global:mode = Read-Host
 test
 }
@@ -12,14 +13,13 @@ test
 function test(){
 if ($mode -eq 1){ipconfig}
 elseif ($mode -eq 2){ipconfig /all}
+elseif ($mode -eq 3){ipconfig /flushdns}
 "What do you want to do? type the number"
-"1) Refresh"
+"1) Ipconfig again"
 "2) Change Settings"
 "3) Exit"
 $einput = Read-Host
-if ( 1 -eq $einput ){
-clear
-test}
+if ( 1 -eq $einput ){test}
 elseif ( 2 -eq $einput ){startP}
 elseif ( 3 -eq $einput ){exit}
 }
