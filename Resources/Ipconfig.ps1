52f49534598fb65d61e44cd9f4951c05b7c5fe46
ExtremePro11299 <134209046+ExtremePro11299@users.noreply.github.com>
2024-01-02 10:00:35 +0100

    Added clear to all scripts (#2)
    The scripts now use the clear command to reduce visual clutter.

--- Resources/Ipconfig.ps1
+++ Resources/Ipconfig.ps1
@@ -1,27 +1,28 @@
 function startP()
 {
-"How would you like to Ipconfig?"
-"1) Show info"
-"2) Show detailed info"
-"3) Flush DNS resolver cache"
-$global:mode = Read-Host
-test
+    "How would you like to Ipconfig?"
+    "1) Show info"
+    "2) Show detailed info"
+    "3) Flush DNS resolver cache"
+    $global:mode = Read-Host
+    test
 }
 
 
 
 function test(){
-if ($mode -eq 1){ipconfig}
-elseif ($mode -eq 2){ipconfig /all}
-elseif ($mode -eq 3){ipconfig /flushdns}
-"What do you want to do? type the number"
-"1) Ipconfig again"
-"2) Change Settings"
-"3) Exit"
-$einput = Read-Host
-if ( 1 -eq $einput ){test}
-elseif ( 2 -eq $einput ){startP}
-elseif ( 3 -eq $einput ){exit}
+	clear
+    if ($mode -eq 1){ipconfig}
+    elseif ($mode -eq 2){ipconfig /all}
+    elseif ($mode -eq 3){ipconfig /flushdns}
+    "What do you want to do? type the number"
+    "1) Ipconfig again"
+    "2) Change Settings"
+    "3) Exit"
+    $einput = Read-Host
+    if ( 1 -eq $einput ){test}
+    elseif ( 2 -eq $einput ){startP}
+    elseif ( 3 -eq $einput ){exit}
 }
 $host.UI.RawUI.WindowTitle = "Ipconfig module"
 startP
